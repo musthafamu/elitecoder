3668ab02e36c6d217114484f892ecc2fdd31aa40
musthafa.id78 <musthafa.id78@gmail.com>
2025-06-02 22:17:34 +0530

    chnages

--- src/components/course/Courses.jsx
+++ src/components/course/Courses.jsx
@@ -16,17 +16,17 @@ function Courses() {
       fee: 25000 
     },
   ];
-
+  
   return (
     <div className=" bg-gradient-to-br p-8  border-t">
       <h1 className="text-5xl font-bold mb-8 text-center text-green-600 ">Featured Courses</h1>
-      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid  grid-cols-1 md:grid-cols-2 shadow-md lg:grid-cols-3 gap-6">
         {courseList.map((course, index) => {
           const Icon = course.icon;
           return (
             <div
               key={index}
-              classN ame="bg-gradient-to-br from-green-50 to-white  p-6 rounded-lg shadow-md hover:shadow-xl transition-all duration-300 transform hover:scale-105"
+              className="bg-white border shadow-md border-gray-300  rounded-md   transition-all duration-300 transform hover:scale-200 p-4"
             >
               <div className="flex items-center mb-4">
                 <div className='bg-green-500 w-12 h-12 rounded-full flex justify-center items-center mr-4'>
@@ -37,7 +37,7 @@ function Courses() {
               <p className="text-green-600 font-medium mb-4">Duration: {course.duration}</p>
               <p className="text-green-600 font-medium mb-4">Technology: {course.technology}</p>
               <p className="text-green-600 font-medium mb-4">Fee: ₹{course.fee}</p>
-              <button className=" p-2 rounded-md bg-green-500 hover:bg-green-600 text-white">
+              <button className="p-3 rounded-md bg-green-600 hover:bg-green-700 text-white font-bold transition duration-200">
                 Register Now
               </button>
             </div>
